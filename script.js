08de5a643b38ea268d0b03274129e3f1d474de17
Logeshwaran N <117084202+Logeshwaran29@users.noreply.github.com>
2022-11-05 15:34:22 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function valid(){
+    let fn=document.getElementById("fname").value;
+    let ln=document.getElementById("lname").value;
+    let number=document.getElementById("phno").value;
+    let mail=document.getElementById("email").value;
+    let a=document.getElementById("pass").value;
+    let b=document.getElementById("cpass").value;
+    var valid=true;
+    
+    
+}
